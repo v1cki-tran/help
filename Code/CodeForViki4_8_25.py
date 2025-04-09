57d7354a8c813c43dd98e35e94f83b55c9afe71c
Reid Taylor <reidtaylor1126@gmail.com>
2025-04-09 02:00:09 -0400

    more QoL improvements

--- Code/CodeForViki4_8_25.py
+++ Code/CodeForViki4_8_25.py
@@ -13,40 +13,33 @@ import re
 from cloud_motion import cloud_motion
 from cloud_detection import cloud_detection
 
-IMG_DIR = 'set4'
+IMG_DIR = 'set5'
+
+def datetime_from_filename(filename: str) -> datetime.datetime:
+    return datetime.datetime(*[int(n) for n in re.split(r'[-_.]', filename)[:-1]])
 
 folder = os.listdir(IMG_DIR)
-# determine the start date based on the first filename
-start_date = [int(n) for n in re.split(r'[-_.]', folder[0])[:-1]]
-
-now_list = []
-a = datetime.datetime(*start_date)
-
-# generate 
-for i in range(len(folder)):
-    now_list.append(a + datetime.timedelta(seconds=30*i))
-
+a = datetime_from_filename(folder[0])
 
 full_array=[]
 seg_frame1,__,____,_____=cloud_detection(f'{IMG_DIR}/2025-03-22_22-50-03.jpg')
-
-
-
-j = 0
-for images in folder:
-    image_filename=IMG_DIR+'/'+ images
+i = 0
+for image_name in folder[1:]: # skip the first image since we grabbed it for the first seg_frame1
+    image_path = IMG_DIR + '/' + image_name
     csv_array=[]
-    seg_frame2, percent_cover, file_name, motion_mask = cloud_detection(image_filename)
-    csv_array=cloud_motion(seg_frame1, seg_frame2, percent_cover, image_filename, 
-                 now_list[j], csv_array, motion_mask)
+    seg_frame2, percent_cover, file_name, motion_mask = cloud_detection(image_path)
+    csv_array=cloud_motion(seg_frame1, seg_frame2, percent_cover, image_path, 
+                datetime_from_filename(image_name) , csv_array, motion_mask)
     seg_frame1=seg_frame2
     full_array.append(csv_array[0])
-    j=j+1
-    print(f"{j}/{len(folder)} images processed ({round(100*j/len(folder))}%)...", end='\r')
+    i += 1
+    print(f"\r{i}/{len(folder)} images processed ({round(100*i/len(folder))}%)...", end='')
     if np.isnan(csv_array[0][3]):
         print("NaN alert!!")
         break
-    
+
+print('\ndickskucekr', end='\r')
+
 listical = [[],
             [],
             [],
@@ -65,7 +58,7 @@ plt.title('Percent coverage(%)')
 plt.tick_params(pad=10)
 plt.xticks(fontsize=16, rotation=65)
 plt.yticks(fontsize=14)
-# plt.locator_params(axis='x', nbins=5)
+plt.ylim(0, 100)
 plt.xlabel("Time")
 plt.ylabel("Percent coverage (%)")
 plt.grid(True)
@@ -77,10 +70,7 @@ plt.title('Average magnitude')
 plt.tick_params(pad=10)
 plt.xticks(fontsize=16, rotation=65)
 plt.yticks(fontsize=14)
-# plt.locator_params(axis='x', nbins=5)
 plt.xlabel("Time")
 plt.ylabel("Average Magnitude")
 plt.grid(True)
 plt.savefig(f"{IMG_DIR}_Plot2.png")
-
-print('Done!')
